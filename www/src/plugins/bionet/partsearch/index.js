ef7b08705067dd2e84fe7e5070e865d76b52a53b
Marc Juul <github@juul.io>
2016-11-22 08:42:02 -0800

    virtuals search in progress

--- www/src/plugins/bionet/partsearch/index.js
+++ www/src/plugins/bionet/partsearch/index.js
@@ -8,6 +8,7 @@ var partsearch = {
     // search
     require('./search-result.tag.html')
 
+    // TODO are these still used?
     app.addStream('bioClassQuery')
     app.addStream('bioInstanceQuery')
     app.addStream('bioPhysicalQuery')
@@ -15,10 +16,9 @@ var partsearch = {
     const searchCache = app.addStream('searchCache').init(undefined)
     const bioClassCache = app.addStream('bioClassCache').init(undefined)
     const bioInstanceCache = app.addStream('bioInstanceCache').init(undefined)
-//           secondary_url: '#!/q?partid=' + x.id,
 
-    searchResult.reduceRoute('class', (m, streamItem) => {
-      const mapItem = function (x) {
+
+/*
         return {
           url: '/edit/' + x.id,
           secondary_url: '/q?partid=' + x.id,
@@ -28,101 +28,7 @@ var partsearch = {
           secondary_text: x.description,
           starred: false,
           data: x.data
-        }
-      }
-      if (Array.isArray(streamItem)) {
-        console.log('search result reducer, array:')
-        const itemList = []
-        for (var i = 0; i < streamItem.length; i++) {
-          itemList.push(mapItem(streamItem[i]))
-        }
-        return itemList
-      } else {
-        const item = mapItem(streamItem)
-          //console.log('searchResult reduce:', JSON.stringify(item))
-        return item
-          //return mapItem(streamItem)
-      }
-    })
-
-    searchResult.reduceRoute('instance', (m, streamItem) => {
-      const mapItem = function (x) {
-        var distance = (x.distance !== undefined) ? ' ' + Math.round(x.distance) + 'km' : ''
-        if (distance === ' 0km') distance = ' ***available in lab'
-          //url: '#!/scan?id=' + x.id,
-        return {
-          url: '/scan?id=' + x.id,
-          righticon: 'star',
-          primary_text: x.locationid,
-          secondary_text: x.id + distance,
-          starred: false,
-          data: x
-        }
-      }
-      if (Array.isArray(streamItem)) {
-        console.log('search result reducer, array:')
-        const itemList = []
-        for (var i = 0; i < streamItem.length; i++) {
-          itemList.push(mapItem(streamItem[i]))
-        }
-        return itemList
-      } else {
-        //console.log('searchResult instance reducer:', JSON.stringify(streamItem))
-        const item = mapItem(streamItem)
-          //console.log('searchResult instance reducer:', JSON.stringify(item))
-        return item
-          //return mapItem(streamItem)
-      }
-    })
-
-    app.observe('bioClassQuery', (q) => {
-      app.getStream('searchCache').init(undefined)
-      bionetapi.partClassSearch(q.q, (result, data, msg) => {
-        //console.log('search callback: result:%b data:%s', result, JSON.stringify(data, null, 2))
-        if (result) {
-          //app.route('searchResult', 'end')
-          app.route('searchResult', 'list', 'class', data)
-          app.dispatch('bioClassCache', searchResult.getModel())
-        } else {
-          app.getThemeMethod().toast(msg)
-        }
-      })
-    })
-
-    app.observe('bioInstanceQuery', (q) => {
-
-      // initialize search result stream
-      //app.route('searchResult', 'start')
-      app.getStream('searchCache').init(undefined)
-
-      // initiate query
-      //if (q.partid !== undefined) {
-        bionetapi.partInstanceSearch(q, (result, data, msg) => {
-          if (result) {
-            //app.route('searchResult', 'end')
-            app.route('searchResult', 'list', 'instance', data)
-            app.dispatch('bioInstanceCache', searchResult.getModel())
-          } else {
-            app.getThemeMethod().toast(msg)
-          }
-        })
-      //}
-    })
-    
-    app.observe('bioPhysicalQuery', (q) => {
-
-      // initialize search result stream
-      app.getStream('searchCache').init(undefined)
-
-      bionetapi.partPhysicalSearch(q, (result, data, msg) => {
-        if (result) {
-          app.route('searchResult', 'list', 'physical', data)
-          app.dispatch('bioInstanceCache', searchResult.getModel())
-        } else {
-          app.getThemeMethod().toast(msg)
-        }
-      })
-    })
+*/
 
     //---------------------------------------------------------------------
     // search route
@@ -138,42 +44,19 @@ var partsearch = {
       if (q.page !== undefined) {
         console.log('search result page: ' + q.page)
       }
-      const terms = (q.terms !== undefined) ? '?terms=' + q.terms : ''
+      const termsURL = (q.terms !== undefined) ? '?terms=' + q.terms : ''
 
-      if (q.partid !== undefined) {
-        const distance = (q.distance === undefined) ? '500km' : q.distance
-        const lab = (q.lab === undefined) ? 'LAB' : q.lab
-        const qp = {
-          q: q.partid,
-          partid: q.partid,
-          locationid: lab,
-          distance: distance
-        }
-        app.dispatch('bioInstanceQuery', qp)
-        app.dispatch('searchCache', bioInstanceCache.getModel())
-        app.dispatch(app.$.breadcrumbs, [{
-            'label': 'search',
-            'url': '#!/'
-        }, {
-            'label': app.getModel('bioClassQuery').q,
-            'url': '#!/q' + terms
-        }, {
-            'label': q.partid,
-            'url': '#!/q?partid=' + q.partid
-        }
-        ]);
-      } else {
-        app.dispatch('searchCache', bioClassCache.getModel())
-        app.dispatch(app.$.breadcrumbs, [{
-            'label': 'search',
-            'url': '#!/'
-        }, {
-            'label': app.getModel('bioClassQuery').q,
-            'url': '#!/q' + terms
-        }
-        ]);
-      }
-      riot.mount('div#content', 'search-result')
+      q.terms = decodeURIComponent(q.terms)
+
+      app.dispatch(app.$.breadcrumbs, [{
+        'label': 'search',
+        'url': '/'
+      }, {
+        'label': q.terms,
+        'url': '/q' + termsURL
+      }]);
+
+      riot.mount('div#content', 'search-result', q)
     })
   },
   remove: function() {
