2eb6d3ca0e14218c51a486980aaa439867cd0650
autonomous <autonomous666@gmail.com>
2016-12-16 00:09:08 -0800

    implemented ui f for adding and viewing list of bio-physicals

--- www/src/plugins/bionet/partsearch/index.js
+++ www/src/plugins/bionet/partsearch/index.js
@@ -1,84 +1,140 @@
-const riot=require('riot')
+const riot = require('riot')
 import bionetapi from '../bionetapi'
 
 var partsearch = {
-  init: function () {
-    
-    //-------------------------------------------------------------------------
-    // search
-    require('./search-result.tag.html')
+    init: function () {
 
-    // TODO are these still used?
-    app.addStream('bioClassQuery')
-    app.addStream('bioInstanceQuery')
-    app.addStream('bioPhysicalQuery')
-    const searchResult = app.addStreamRouter('searchResult')
-    const searchCache = app.addStream('searchCache').init(undefined)
-    const bioClassCache = app.addStream('bioClassCache').init(undefined)
-    const bioInstanceCache = app.addStream('bioInstanceCache').init(undefined)
+        //-------------------------------------------------------------------------
+        // search
+        require('./search-result.tag.html')
 
+        // TODO are these still used?
+        app.addStream('bioClassQuery')
+        app.addStream('bioInstanceQuery')
+        const bioPhysicalQuery = app.addStream('bioPhysicalQuery')
+        const searchResult = app.addStreamRouter('searchResult')
+        const searchCache = app.addStream('searchCache').init(undefined)
+        const bioClassCache = app.addStream('bioClassCache').init(undefined)
+        const bioInstanceCache = app.addStream('bioInstanceCache').init(undefined)
 
-/*
-        return {
-          url: '/edit/' + x.id,
-          secondary_url: '/q?partid=' + x.id,
-          secondary_url_label: 'view availabilty',
-          righticon: 'star',
-          primary_text: x.id,
-          secondary_text: x.description,
-          starred: false,
-          data: x.data
-*/
+        bioPhysicalQuery.reduceRoute('toQueryItem', (m, partData) => {
+            return {
+                id: partData.datamatrix,
+                name: partData.name,
+                cassetteid: partData.cassetteid,
+                locationid: partData.locationid
+            }
+        })
 
-    //---------------------------------------------------------------------
-    // search route
-    app.addRoute('/q..', function () {
-      app.dispatch(app.$.appBarConfig, {
-        enableTopNav: true,
-        enableBreadCrumbs: true,
-        enableSubbar: false
-      })
+        app.observe('bioPhysicalQuery', (q) => {
+            app.remote.inventoryTree(function (err, children) {
+                if (err) return console.error(err);
 
-      // todo: handle pagination
-      const q = route.query()
-      if (q.page !== undefined) {
-        console.log('search result page: ' + q.page)
-      }
-      const termsURL = (q.terms !== undefined) ? '?terms=' + q.terms : ''
+                // TODO rewrite this matching algorithm 
+                //      so we can do single-pass matching
+                //      so we can use a stream
+                //      And move it to the level-tree-index module
 
-      q.terms = decodeURIComponent(q.terms)
+                var matches = [];
+                var nodes = [];
 
-      app.dispatch(app.$.breadcrumbs, [{
-        'label': 'search',
-        'url': '/'
+                var i, cur, indent, a;
+                for (i = 0; i < children.length; i++) {
+                    cur = children[i].path;
+                    if (cur.match(q)) matches.push(cur);
+                }
+
+                var j, m, add, perfect;
+                for (i = 0; i < children.length; i++) {
+                    cur = children[i].path;
+                    a = cur.split('.');
+                    indent = a.length - 1;
+                    add = false
+                    perfect = false
+                    for (j = 0; j < matches.length; j++) {
+                        m = matches[j];
+                        if (m.indexOf(cur) === 0) {
+                            add = true;
+                            if (m.length === cur.length) perfect = true;
+                            break;
+                        }
+                    }
+                    if (add) {
+                        const bioPhysical = {
+                            indent: indent,
+                            url: 'edit-physical/' + children[i].path + "/?id=" + children[i].key,
+                            primary_text: a[a.length - 1],
+                            id: children[i].key,
+                            highlight: perfect
+                        };
+                        nodes.push(bioPhysical)
+                    }
+                }
+                app.route('searchResult', 'list', null, nodes)
+            });
+        })
+
+        /*
+                return {
+                  url: '/edit/' + x.id,
+                  secondary_url: '/q?partid=' + x.id,
+                  secondary_url_label: 'view availabilty',
+                  righticon: 'star',
+                  primary_text: x.id,
+                  secondary_text: x.description,
+                  starred: false,
+                  data: x.data
+        */
+
+        //---------------------------------------------------------------------
+        // search route
+        app.addRoute('/q..', function () {
+            app.dispatch(app.$.appBarConfig, {
+                enableTopNav: true,
+                enableBreadCrumbs: true,
+                enableSubbar: false
+            })
+
+            // todo: handle pagination
+            const q = route.query()
+            if (q.page !== undefined) {
+                console.log('search result page: ' + q.page)
+            }
+            const termsURL = (q.terms !== undefined) ? '?terms=' + q.terms : ''
+
+            q.terms = decodeURIComponent(q.terms)
+
+            app.dispatch(app.$.breadcrumbs, [{
+                'label': 'search',
+                'url': '/'
       }, {
-        'label': q.terms,
-        'url': '/q' + termsURL
+                'label': q.terms,
+                'url': '/q' + termsURL
       }]);
 
-      setTimeout(function() { // TODO remove timeout
+            setTimeout(function () { // TODO remove timeout
 
-        app.remote.elasticSearch(q.terms, function(err, results) {
-          if(err) return console.error(err); // TODO handle error better
-          
-          q.results = [];
-          var i;
-          for(i=0; i < results.length; i++) {
-            if(!results[i]._source || !results[i]._source.name) continue;
-            q.results.push({
-              primary_text: results[i]._source.name,
-              id: results[i]._source.id
-            });
-          }
+                app.remote.elasticSearch(q.terms, function (err, results) {
+                    if (err) return console.error(err); // TODO handle error better
 
-          riot.mount('div#content', 'search-result', q)
-          
-        });
-      }, 500);
-    })
-  },
-  remove: function() {
-    
-  }
+                    q.results = [];
+                    var i;
+                    for (i = 0; i < results.length; i++) {
+                        if (!results[i]._source || !results[i]._source.name) continue;
+                        q.results.push({
+                            primary_text: results[i]._source.name,
+                            id: results[i]._source.id
+                        });
+                    }
+
+                    riot.mount('div#content', 'search-result', q)
+
+                });
+            }, 500);
+        })
+    },
+    remove: function () {
+
+    }
 }
 module.exports = partsearch
